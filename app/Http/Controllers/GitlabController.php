dbc3095ac3a7a3496e3e597d1f65145699fb8efc
Erickson Reyes <erickson.reyes@sourcefit.com>
2022-02-24 23:46:01 +0800

    Add placeholder API response for user search

--- app/Http/Controllers/GitlabController.php
+++ app/Http/Controllers/GitlabController.php
@@ -3,6 +3,11 @@
 
 namespace App\Http\Controllers;
 
+use Exception;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use ReflectionException;
+
 /**
  * Class GitlabController
  * @package App\Http\Controllers
@@ -10,4 +15,43 @@ namespace App\Http\Controllers;
 class GitlabController extends Controller
 {
 
+    /**
+     * @param Request $request
+     * @return Response
+     * @throws ReflectionException
+     */
+    public function indexAction(Request $request): Response
+    {
+        try {
+            $response = [
+                '_embedded' => [
+                    'users' => [
+                        [
+                            "id" => 1,
+                            "login" => "octocat",
+                            "name" => "monalisa octocat",
+                            "email" => "monalisa.octocat@github.com",
+                            "company" => "Github",
+                            "followers" => 1,
+                            "public_repository_count" => 1,
+                            "average_number_of_public_repository_followers" => 12
+                        ],
+                        [
+                            "id" => 2,
+                            "login" => "ericksonreyes",
+                            "name" => "erickson reyes",
+                            "email" => "ercbluemonday@yahoo.com",
+                            "company" => "Github",
+                            "followers" => 1,
+                            "public_repository_count" => 5,
+                            "average_number_of_public_repository_followers" => 2
+                        ]
+                    ]
+                ]
+            ];
+            return \response($response, Response::HTTP_OK);
+        } catch (Exception $exception) {
+            return $this->exception($exception);
+        }
+    }
 }
