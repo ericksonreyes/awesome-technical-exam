4518a66bc722b05aa54db9a5d4aef1bcef9072e9
Erickson Reyes <erickson.reyes@sourcefit.com>
2022-02-24 13:08:37 +0800

    Write and pass unit tests for MismatchedPasswordsException

---
+++ src/Github/Domain/Model/Exception/MismatchedPasswordsException.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Github\Domain\Model\Exception;
+
+use InvalidArgumentException;
+
+/**
+ * Class MismatchedPasswordsException
+ * @package Github\Domain\Model\Exception
+ */
+class MismatchedPasswordsException extends InvalidArgumentException
+{
+    protected $message = 'Passwords does not match.';
+}
