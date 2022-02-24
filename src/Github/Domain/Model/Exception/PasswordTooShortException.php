78b3afac5f0d357ddca8d0411797f67c1216c32e
Erickson Reyes <erickson.reyes@sourcefit.com>
2022-02-24 14:38:29 +0800

    Write and pass unit tests for password length validating user registration handler decorator class

---
+++ src/Github/Domain/Model/Exception/PasswordTooShortException.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Github\Domain\Model\Exception;
+
+use InvalidArgumentException;
+
+/**
+ * Class PasswordTooShortException
+ * @package Github\Domain\Model\Exception
+ */
+class PasswordTooShortException extends InvalidArgumentException
+{
+    protected $message = 'Password is too short.';
+}
